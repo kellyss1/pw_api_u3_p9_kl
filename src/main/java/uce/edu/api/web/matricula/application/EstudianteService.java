74445b7c2856f45a46b4f2f4feb03d7661c025ba
kellyss1 <kdledesma@uce.edu.ec>
2026-01-21 11:52:18 -0500

    Taller 26 Verbos HTTP

--- src/main/java/uce/edu/api/web/matricula/application/EstudianteService.java
+++ src/main/java/uce/edu/api/web/matricula/application/EstudianteService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import uce.edu.api.web.matricula.domain.Estudiante;
 import uce.edu.api.web.matricula.infraestructure.EstudianteRepository;
 
@@ -15,5 +16,43 @@ public class EstudianteService {
 
     public List<Estudiante> listarTodos() {
         return this.estudianteRepository.listAll();
+    }
+
+    public Estudiante consultarPorId(Integer id) {
+        return this.estudianteRepository.findById(id.longValue());
+    }
+
+    @Transactional
+    public void crear(Estudiante estu) {
+        this.estudianteRepository.persist(estu);
+    }
+
+    @Transactional
+    public void actualizar(Integer id, Estudiante est) {
+        Estudiante estu = this.consultarPorId(id);
+        estu.apellido = est.apellido;
+        estu.nombre = est.nombre;
+        estu.fechaNacimiento = est.fechaNacimiento;
+        // Se actualiza automaticamente por dirty checking
+    }
+
+    @Transactional
+    public void actualizarParcial(Integer id, Estudiante est) {
+        Estudiante estu = this.consultarPorId(id);
+        if (est.nombre != null) {
+            estu.nombre = est.nombre;
+        }
+        if (est.apellido != null) {
+            estu.apellido = est.apellido;
+        }
+        if (est.fechaNacimiento != null) {
+            estu.fechaNacimiento = est.fechaNacimiento;   
+        }
+        // Se actualiza automaticamente por dirty checking
+    }
+
+    @Transactional
+    public void eliminar(Integer id) {
+        this.estudianteRepository.deleteById(id.longValue());
     }
 }
--- src/main/java/uce/edu/api/web/matricula/interfaces/EstudianteResource.java
+++ src/main/java/uce/edu/api/web/matricula/interfaces/EstudianteResource.java
@@ -3,8 +3,13 @@ package uce.edu.api.web.matricula.interfaces;
 import java.util.List;
 
 import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import uce.edu.api.web.matricula.application.EstudianteService;
 import uce.edu.api.web.matricula.domain.Estudiante;
 
@@ -18,5 +23,35 @@ public class EstudianteResource {
     @Path("/todos")
     public List<Estudiante> listarTodos() {
         return this.estudianteService.listarTodos();
+    }
+
+    @GET
+    @Path("/consultarPorId/{id}")
+    public Estudiante consultarPorId(@PathParam("id") Integer id) {
+        return this.estudianteService.consultarPorId(id);
+    }
+
+    @POST
+    @Path("/crear")
+    public void guardar(Estudiante estu) {
+        this.estudianteService.crear(estu);
+    }
+
+    @PUT
+    @Path("actualizar/{id}")
+    public void actualizar(@PathParam("id") Integer id, Estudiante estu) {
+        this.estudianteService.actualizar((id), estu);
+    }
+
+    @PATCH
+    @Path("/actualizarParcial/{id}")
+    public void actualizarParcial(@PathParam("id") Integer id, Estudiante estu) {
+        this.estudianteService.actualizarParcial(id, estu);
+    }
+
+    @DELETE
+    @Path("/borrar/{id}")
+    public void borrar(@PathParam("id") Integer id) {
+        this.estudianteService.eliminar(id);
     }
 }
